b3c83a80cd47618e867a2b8b84a2303f795eae1e
chaudi <cederfelt@live.com>
2015-12-19 14:44:41 +0100

    added bmp280 class

---
+++ IotDeviceLibrary/BMP280.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Enumeration;
+using Windows.Devices.I2c;
+
+namespace IotDeviceLibrary
+{
+    class BMP280_CalibrationData
+    {
+        //BMP280 Registers
+        public UInt16 DigT1 { get; set; }
+        public Int16 DigT2 { get; set; }
+        public Int16 DigT3 { get; set; }
+
+        public UInt16 DigP1 { get; set; }
+        public Int16 DigP2 { get; set; }
+        public Int16 DigP3 { get; set; }
+        public Int16 DigP4 { get; set; }
+        public Int16 DigP5 { get; set; }
+        public Int16 DigP6 { get; set; }
+        public Int16 DigP7 { get; set; }
+        public Int16 DigP8 { get; set; }
+        public Int16 DigP9 { get; set; }
+    }
+
+
+    class BMP280
+    {
+        //The BMP280 register addresses according the the datasheet: http://www.adafruit.com/datasheets/BST-BMP280-DS001-11.pdf
+        const byte BMP280_Address = 0x77;
+        const byte BMP280_Signature = 0x58;
+
+        enum eRegisters : byte
+        {
+            BMP280_REGISTER_DIG_T1 = 0x88,
+            BMP280_REGISTER_DIG_T2 = 0x8A,
+            BMP280_REGISTER_DIG_T3 = 0x8C,
+
+            BMP280_REGISTER_DIG_P1 = 0x8E,
+            BMP280_REGISTER_DIG_P2 = 0x90,
+            BMP280_REGISTER_DIG_P3 = 0x92,
+            BMP280_REGISTER_DIG_P4 = 0x94,
+            BMP280_REGISTER_DIG_P5 = 0x96,
+            BMP280_REGISTER_DIG_P6 = 0x98,
+            BMP280_REGISTER_DIG_P7 = 0x9A,
+            BMP280_REGISTER_DIG_P8 = 0x9C,
+            BMP280_REGISTER_DIG_P9 = 0x9E,
+
+            BMP280_REGISTER_CHIPID = 0xD0,
+            BMP280_REGISTER_VERSION = 0xD1,
+            BMP280_REGISTER_SOFTRESET = 0xE0,
+
+            BMP280_REGISTER_CAL26 = 0xE1,  // R calibration stored in 0xE1-0xF0
+
+            BMP280_REGISTER_CONTROLHUMID = 0xF2,
+            BMP280_REGISTER_CONTROL = 0xF4,
+            BMP280_REGISTER_CONFIG = 0xF5,
+
+            BMP280_REGISTER_PRESSUREDATA_MSB = 0xF7,
+            BMP280_REGISTER_PRESSUREDATA_LSB = 0xF8,
+            BMP280_REGISTER_PRESSUREDATA_XLSB = 0xF9, // bits <7:4>
+
+            BMP280_REGISTER_TEMPDATA_MSB = 0xFA,
+            BMP280_REGISTER_TEMPDATA_LSB = 0xFB,
+            BMP280_REGISTER_TEMPDATA_XLSB = 0xFC, // bits <7:4>
+
+            BMP280_REGISTER_HUMIDDATA_MSB = 0xFD,
+            BMP280_REGISTER_HUMIDDATA_LSB = 0xFE,
+        };
+
+
+        private const string I2CControllerName = "I2C1";
+
+        private I2cDevice _bmp280 = null;
+
+        BMP280_CalibrationData _calibrationData;
+
+        private bool _init = false;
+
+        public async Task Initialize()
+        {
+            Debug.WriteLine("BMP280 initialized");
+            try
+            {
+                I2cConnectionSettings settings = new I2cConnectionSettings(BMP280_Address);
+
+                settings.BusSpeed = I2cBusSpeed.FastMode;
+
+                string aqs = I2cDevice.GetDeviceSelector(I2CControllerName);
+
+                DeviceInformationCollection dis = await DeviceInformation.FindAllAsync(aqs);
+
+                _bmp280 = await I2cDevice.FromIdAsync(dis[0].Id, settings);
+
+                if (_bmp280 == null)
+                {
+                    Debug.WriteLine("Device not found");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Exception: " + e.Message + "\n" + e.StackTrace);
+                throw;
+            }
+        }
+
+        private async Task Begin()
+        {
+            Debug.WriteLine("BMP28::BEGIN");
+            byte[] writeBuffer = new byte[] { (byte)eRegisters.BMP280_REGISTER_CHIPID };
+            byte[] readBuffer = new byte[] { 0xFF };
+
+            _bmp280.WriteRead(writeBuffer, readBuffer);
+            Debug.WriteLine("BMP280 Signature: " + readBuffer[0].ToString());
+
+            if (readBuffer[0] != BMP280_Signature)
+            {
+                {
+                    Debug.WriteLine("BMP280::Begin Signature Mismatch.");
+                    return;
+                }
+            }
+            _init = true;
+
+            //Read the coefficients table
+            _calibrationData = await ReadCoefficeints();
+
+            //Write control register
+            await WriteControlRegister();
+
+            //Write humidity control register
+            await WriteControlRegisterHumidity();
+        }
+
+        //Method to write 0x03 to the humidity control register
+        private async Task WriteControlRegisterHumidity()
+        {
+            byte[] writeBuffer = new byte[] { (byte)eRegisters.BMP280_REGISTER_CONTROLHUMID, 0x03 };
+            _bmp280.Write(writeBuffer);
+            await Task.Delay(1);
+            return;
+        }
+
+        //Method to write 0x3F to the control register
+        private async Task WriteControlRegister()
+        {
+            byte[] writeBuffer = new byte[] { (byte)eRegisters.BMP280_REGISTER_CONTROL, 0x3F };
+            _bmp280.Write(writeBuffer);
+            await Task.Delay(1);
+            return;
+        }
+
+        //Method to read a 16-bit value from a register and return it in little endian format
+        private UInt16 ReadUInt16_LittleEndian(byte register)
+        {
+            UInt16 value = 0;
+            byte[] writeBuffer = new byte[] { 0x00 };
+            byte[] readBuffer = new byte[] { 0x00, 0x00 };
+
+            writeBuffer[0] = register;
+
+            _bmp280.WriteRead(writeBuffer, readBuffer);
+            int h = readBuffer[1] << 8;
+            int l = readBuffer[0];
+            value = (UInt16)(h + l);
+            return value;
+        }
+
+        //Method to read an 8-bit value from a register
+        private byte ReadByte(byte register)
+        {
+            byte value = 0;
+            byte[] writeBuffer = new byte[] { 0x00 };
+            byte[] readBuffer = new byte[] { 0x00 };
+
+            writeBuffer[0] = register;
+
+            _bmp280.WriteRead(writeBuffer, readBuffer);
+            value = readBuffer[0];
+            return value;
+        }
+
+        public async Task<float> ReadTemperature()
+        {
+            //Make sure the I2C device is initialized
+            if (!_init) await Begin();
+
+            //Read the MSB, LSB and bits 7:4 (XLSB) of the temperature from the BMP280 registers
+            byte tmsb = ReadByte((byte)eRegisters.BMP280_REGISTER_TEMPDATA_MSB);
+            byte tlsb = ReadByte((byte)eRegisters.BMP280_REGISTER_TEMPDATA_LSB);
+            byte txlsb = ReadByte((byte)eRegisters.BMP280_REGISTER_TEMPDATA_XLSB); // bits 7:4
+
+            //Combine the values into a 32-bit integer
+            Int32 t = (tmsb << 12) + (tlsb << 4) + (txlsb >> 4);
+
+            //Convert the raw value to the temperature in degC
+            double temp = BMP280_compensate_T_double(t);
+
+            //Return the temperature as a float value
+            return (float)temp;
+        }
+
+        public async Task<float> ReadPreasure()
+        {
+            //Make sure the I2C device is initialized
+            if (!_init) await Begin();
+
+            //Read the temperature first to load the t_fine value for compensation
+            if (t_fine == Int32.MinValue)
+            {
+                await ReadTemperature();
+            }
+
+            //Read the MSB, LSB and bits 7:4 (XLSB) of the pressure from the BMP280 registers
+            byte tmsb = ReadByte((byte)eRegisters.BMP280_REGISTER_PRESSUREDATA_MSB);
+            byte tlsb = ReadByte((byte)eRegisters.BMP280_REGISTER_PRESSUREDATA_LSB);
+            byte txlsb = ReadByte((byte)eRegisters.BMP280_REGISTER_PRESSUREDATA_XLSB); // bits 7:4
+
+            //Combine the values into a 32-bit integer
+            Int32 t = (tmsb << 12) + (tlsb << 4) + (txlsb >> 4);
+
+            //Convert the raw value to the pressure in Pa
+            Int64 pres = BMP280_compensate_P_Int64(t);
+
+            //Return the temperature as a float value
+            return ((float)pres) / 256;
+        }
+
+        //Method to take the sea level pressure in Hectopascals(hPa) as a parameter and calculate the altitude using current pressure.
+        public async Task<float> ReadAltitude(float seaLevel)
+        {
+            //Make sure the I2C device is initialized
+            if (!_init) await Begin();
+
+            //Read the pressure first
+            float pressure = await ReadPreasure();
+            //Convert the pressure to Hectopascals(hPa)
+            pressure /= 100;
+
+            //Calculate and return the altitude using the international barometric formula
+            return 44330.0f * (1.0f - (float)Math.Pow((pressure / seaLevel), 0.1903f));
+        }
+
+        //Method to read the caliberation data from the registers
+        private async Task<BMP280_CalibrationData> ReadCoefficeints()
+        {
+            // 16 bit calibration data is stored as Little Endian, the helper method will do the byte swap.
+            _calibrationData = new BMP280_CalibrationData();
+
+            // Read temperature calibration data
+            _calibrationData.DigT1 = ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_T1);
+            _calibrationData.DigT2 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_T2);
+            _calibrationData.DigT3 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_T3);
+
+            // Read presure calibration data
+            _calibrationData.DigP1 = ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P1);
+            _calibrationData.DigP2 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P2);
+            _calibrationData.DigP3 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P3);
+            _calibrationData.DigP4 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P4);
+            _calibrationData.DigP5 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P5);
+            _calibrationData.DigP6 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P6);
+            _calibrationData.DigP7 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P7);
+            _calibrationData.DigP8 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P8);
+            _calibrationData.DigP9 = (Int16)ReadUInt16_LittleEndian((byte)eRegisters.BMP280_REGISTER_DIG_P9);
+
+            await Task.Delay(1);
+            return _calibrationData;
+        }
+
+        //t_fine carries fine temperature as global value
+        Int32 t_fine = Int32.MinValue;
+        //Method to return the temperature in DegC. Resolution is 0.01 DegC. Output value of “5123” equals 51.23 DegC.
+        private double BMP280_compensate_T_double(Int32 adc_T)
+        {
+            double var1, var2, T;
+
+            //The temperature is calculated using the compensation formula in the BMP280 datasheet
+            var1 = ((adc_T / 16384.0) - (_calibrationData.DigT1 / 1024.0)) * _calibrationData.DigT2;
+            var2 = ((adc_T / 131072.0) - (_calibrationData.DigT1 / 8192.0)) * _calibrationData.DigT3;
+
+            t_fine = (Int32)(var1 + var2);
+
+            T = (var1 + var2) / 5120.0;
+            return T;
+        }
+        //Method to returns the pressure in Pa, in Q24.8 format (24 integer bits and 8 fractional bits).
+        //Output value of “24674867” represents 24674867/256 = 96386.2 Pa = 963.862 hPa
+        private Int64 BMP280_compensate_P_Int64(Int32 adc_P)
+        {
+            Int64 var1, var2, p;
+
+            //The pressure is calculated using the compensation formula in the BMP280 datasheet
+            var1 = t_fine - 128000;
+            var2 = var1 * var1 * (Int64)_calibrationData.DigP6;
+            var2 = var2 + ((var1 * (Int64)_calibrationData.DigP5) << 17);
+            var2 = var2 + ((Int64)_calibrationData.DigP4 << 35);
+            var1 = ((var1 * var1 * (Int64)_calibrationData.DigP3) >> 8) + ((var1 * (Int64)_calibrationData.DigP2) << 12);
+            var1 = (((((Int64)1 << 47) + var1)) * (Int64)_calibrationData.DigP1) >> 33;
+            if (var1 == 0)
+            {
+                Debug.WriteLine("BMP280_compensate_P_Int64 Jump out to avoid / 0");
+                return 0; //Avoid exception caused by division by zero
+            }
+            //Perform calibration operations as per datasheet: http://www.adafruit.com/datasheets/BST-BMP280-DS001-11.pdf
+            p = 1048576 - adc_P;
+            p = (((p << 31) - var2) * 3125) / var1;
+            var1 = ((Int64)_calibrationData.DigP9 * (p >> 13) * (p >> 13)) >> 25;
+            var2 = ((Int64)_calibrationData.DigP8 * p) >> 19;
+            p = ((p + var1 + var2) >> 8) + ((Int64)_calibrationData.DigP7 << 4);
+            return p;
+        }
+
+
+    }
+}
